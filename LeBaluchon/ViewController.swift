ccfb5c96cbc55fc15f2a4cd69575851ab427f6fc
Richard Mazid <Richard.mazid@gmail.com>
2022-06-08 19:34:04 +0200

    Initial Commit

---
+++ LeBaluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeBaluchon
+//
+//  Created by Richard Arif Mazid on 08/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
